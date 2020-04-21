f2fccceb565ce589b30b5f307b686e746e81ef1a
Vitamin5188 <64106649+Vitamin5188@users.noreply.github.com>
2020-04-21 23:52:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -30,39 +30,3 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname+"/websiteclone/dhannoo/web2/index.html")
 });
 
-
-app.get('/share/*', function (req, res) {
-  res.redirect("https://play.google.com/store?hl=en");
-  // res.sendFile(__dirname+"/sportsappClone/dhannoo/web2/index.html")
-});
-
-app.get('/:one/:two',function(req,res){
-  console.log(req.params.two)
-  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two);
-
-});
-
-app.get('/:one/:two/:three',function(req,res){
-  console.log("thrree")
-  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three);
-//  console.log(__dirname+"/sportsappClone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three);
-
-});
-
-app.get('/:one/:two/:three/:four',function(req,res){
-  console.log("four")
-  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four);
-
-});
-
-app.get('/:one/:two/:three/:four/:five',function(req,res){
-  console.log("five")
-  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four+"/"+req.params.five);
-
-});
-
-app.get('/:one/:two/:three/:four/:five/:six',function(req,res){
-  console.log("six")
-  res.sendFile(__dirname+"/websiteclone/dhannoo/web2/"+req.params.one+ "/"+req.params.two+"/"+req.params.three+"/"+req.params.four+"/"+req.params.five+"/"+req.params.six);
-
-});
